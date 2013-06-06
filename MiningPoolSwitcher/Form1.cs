0ecefde243266d6d17e363edb958d9c961909536
Iurii <yura.kimon@gmail.com>
2013-06-06 18:06:38 +0300

    1

--- MiningPoolSwitcher/Form1.cs
+++ MiningPoolSwitcher/Form1.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Text.RegularExpressions;
 
 namespace MiningpoolSwitcher
 {
@@ -14,6 +16,20 @@ namespace MiningpoolSwitcher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string htmlCode;
+            using (WebClient client = new WebClient())
+            {
+                //client.DownloadFile("http://yoursite.com/page.html", @"C:\localfile.html");
+
+                htmlCode = client.DownloadString("https://deepbit.net/stats");
+            }
+
+            Regex r = new Regex("/\\w{64}?'>(.*?)</tr");
+            MatchCollection mc = r.Matches(htmlCode);
         }
     }
 }
