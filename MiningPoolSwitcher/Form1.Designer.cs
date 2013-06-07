943dcfc4e1b1e6540321d63b08a5b51ca6fd047a
Iurii <yura.kimon@gmail.com>
2013-06-07 18:19:15 +0300

    Первая рабочая версия для phoenix
    Осуществляет переключение между пулами deepbit.net и 50btc.com. Шары
    льются на deepbit в первые 6 часов нового блока.

--- MiningPoolSwitcher/Form1.Designer.cs
+++ MiningPoolSwitcher/Form1.Designer.cs
@@ -28,12 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(222, 121);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(182, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Проверить deepbit.net";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(392, 100);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "...";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(416, 166);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "MiningPoolSwitcher";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- MiningPoolSwitcher/Form1.cs
+++ MiningPoolSwitcher/Form1.cs
@@ -8,28 +8,160 @@ using System.Text;
 using System.Windows.Forms;
 using System.Net;
 using System.Text.RegularExpressions;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
 
 namespace MiningpoolSwitcher
 {
     public partial class Form1 : Form
     {
+        Thread _thread;
+        ManualResetEvent _stop = new ManualResetEvent(false);
+        public delegate void UpdateFormDelegate();
+
+        DateTime _lastBlockDate = DateTime.MinValue;
+        string _duration;
+
         public Form1()
         {
             InitializeComponent();
+            
+            ReadWritePhoenixConfig();
+            
+            _thread = new Thread(CheckPhoenixByRPC);
+            _thread.Start();
         }
+
+        private void CheckPhoenixByRPC()
+        {
+            do
+            {
+                string htmlCode;
+                using (WebClient client = new WebClient())
+                {
+                    htmlCode = client.DownloadString("https://deepbit.net/stats");
+                }
+
+                Regex r = new Regex("/\\w{64}?'>(.*?)</tr");
+                foreach (Match m in r.Matches(htmlCode))
+                {
+                    string strDate = (new Regex("(.*?)</a")).Match(m.Groups[1].Value).Groups[1].Value.Replace("&nbsp;", " ");
+                    DateTime utcDate = DateTime.ParseExact(strDate, "dd.MM H:mm:ss", null);
+                    _lastBlockDate = utcDate.ToLocalTime();
+                    _duration = (new Regex("<td>(.*?)</td>")).Match(m.Groups[1].Value).Groups[1].Value;
+                    break;
+                }
+
+                try
+                {
+                    this.Invoke(new UpdateFormDelegate(this.ChangeSomeFormControls), new object[] { });
+                }
+                catch { }
+
+                SwitchPoolIfNeed();
+            }
+            while (!_stop.WaitOne(60000));
+        }
+
+        public void ChangeSomeFormControls()
+        {
+            TimeSpan period = DateTime.Now - _lastBlockDate;
+            label1.Text = string.Format("Последний найденый блок на deepbit.net {0} за {1},\r\n"
+                + "c последенго блока прошло {2}h {3}m\r\n\r\nОсновной пул deepbit.net: {4}", 
+                _lastBlockDate, _duration, period.Hours, period.Minutes, isPoolDeepbitMainCurrent ? "Да" : "Нет");
+        }
+
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string htmlCode;
-            using (WebClient client = new WebClient())
-            {
-                //client.DownloadFile("http://yoursite.com/page.html", @"C:\localfile.html");
+            SwitchPoolIfNeed();
+        }
 
-                htmlCode = client.DownloadString("https://deepbit.net/stats");
+        void SwitchPoolIfNeed()
+        {
+            ReadWritePhoenixConfig();
+            if (isPoolDeepbitMainNeed != null && isPoolDeepbitMainCurrent != isPoolDeepbitMainNeed)
+            {
+                ReadWritePhoenixConfig(true);
+                try { Process.Start("rerun.bat"); }
+                catch (Exception ex) { MessageBox.Show(ex.Message); }
+                Log("Основной пул deepbit.net: {0} " + (isPoolDeepbitMainCurrent ? "Да" : "Нет"));
+            }
+            ReadWritePhoenixConfig();
+            this.Invoke(new UpdateFormDelegate(this.ChangeSomeFormControls), new object[] { });
+        }
+
+
+        public static void Log(string s, string logfile = "MiningPoolSwitcher.txt")
+        {
+            //удаляем файл, если он превышает 10 метров
+            FileInfo fi = new FileInfo(logfile);
+            if (fi.Exists && fi.Length > 10000000)
+                fi.Delete();
+
+            File.AppendAllText(logfile, string.Format("{0}: {1}\r\n", DateTime.Now, s));
+        }
+
+        bool isPoolDeepbitMainCurrent; // текущее значение конфига
+
+        bool? isPoolDeepbitMainNeed // на Deepbit прошло менее 6 часов с последнего блока
+        {
+            get
+            {
+                if (_lastBlockDate != DateTime.MinValue)
+                    return DateTime.Now - _lastBlockDate <= new TimeSpan(6, 0, 0);
+                else
+                    return null;
+            }
+        }
+
+
+        void ReadWritePhoenixConfig(bool isWrite = false)
+        {
+            string[] ss = File.ReadAllLines("phoenix.cfg");
+            string[] ss_new = new string[ss.Count()];
+            string acc_deepbit = string.Empty;
+            string acc_50btc = string.Empty;
+
+            //находим строки подключения к пулам
+            foreach (string s in ss)
+            {
+                if (s.Trim().IndexOf("50btc.com") > -1)
+                    acc_50btc = s.Substring(s.IndexOf("=") + 1).Trim();
+                if (s.Trim().IndexOf("deepbit.net") > -1)
+                {
+                    acc_deepbit = s.Substring(s.IndexOf("=") + 1).Trim();
+
+                    //isPoolDeepbitMainCurrent = s.Trim()!= s.Trim().IndexOf("backend") == 0;
+
+                    if (s.Trim().IndexOf("backend") == 0)
+                        isPoolDeepbitMainCurrent = true;
+                    else if (s.Trim().IndexOf("backups") == 0)
+                        isPoolDeepbitMainCurrent = false;
+                }
+
             }
 
-            Regex r = new Regex("/\\w{64}?'>(.*?)</tr");
-            MatchCollection mc = r.Matches(htmlCode);
+            for (int i = 0; i < ss.Count(); i++)
+            {
+                if (ss[i].Trim().IndexOf("backend") == 0)
+                    ss_new[i] = "backend = " + (isPoolDeepbitMainNeed == true ? acc_deepbit : acc_50btc);
+                else if (ss[i].Trim().IndexOf("backups") == 0)
+                    ss_new[i] = "backups = " + (!isPoolDeepbitMainNeed == true ? acc_deepbit : acc_50btc);
+                else
+                    ss_new[i] = ss[i];
+            }
+
+            if (isWrite)
+                File.WriteAllLines("phoenix.cfg", ss_new);
+        }
+
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            _stop.Set();
+            Thread.Sleep(200);
         }
     }
 }
